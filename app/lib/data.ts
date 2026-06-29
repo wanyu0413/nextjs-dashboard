4e3358b1dbfd28ad6e48736ad9f6a5035562ca49
Wanyu Jiang <wanyu19930930@gmail.com>
2026-06-29 12:59:46 +0900

    add loading.tsx

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,15 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
+    // We artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+
+    // console.log('Data fetch completed after 3 seconds.');
+
     return data;
   } catch (error) {
     console.error('Database Error:', error);
