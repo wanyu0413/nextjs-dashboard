4643d46e948ba55913422bd902160139ef1d09ae
Wanyu Jiang <wanyu19930930@gmail.com>
2026-07-06 20:16:10 +0900

    add validation for update invoice with useActionState

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -73,13 +73,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
    
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
     try {
@@ -89,8 +97,6 @@ export async function updateInvoice(id: string, formData: FormData) {
           WHERE id = ${id}
         `;
     } catch (error) {
-        // We'll also log the error to the console for now
-        console.error(error);
         return { message: 'Database Error: Failed to Update Invoice.' }; 
     }
    
