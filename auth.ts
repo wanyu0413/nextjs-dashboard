c40d5c7ab7adc1c352fc42aac11be5dcc731d02d
Wanyu Jiang <wanyu19930930@gmail.com>
2026-08-11 18:43:05 +0900

    adding login & logout functionality

--- auth.ts
+++ auth.ts
@@ -1,12 +1,44 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
  
-// Export the auth, signIn, and signOut functions from the NextAuth library.
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+ 
 export const { auth, signIn, signOut } = NextAuth({
-  // Spread the authConfig object into the NextAuth configuration.
   ...authConfig,
-  // providers is an array where you list different login options such as Google or GitHub.
-  // Here we are using the Credentials provider to login with a username and password.
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          // Compare the password with the hashed password in the database
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+ 
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
 });
